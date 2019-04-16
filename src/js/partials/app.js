ec8371d19b614a1ec5c6a4222fb10cdaf192349c
bohrytsenko <hrytsenkobohdan@gmail.com>
2019-04-16 11:44:25 +0200

    first commit

---
+++ src/js/partials/app.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+
+    // Phone number
+
+    var input = document.querySelector("#phone");
+    window.intlTelInput(input);
+
+    var input = document.querySelector("#recall");
+    window.intlTelInput(input);
+
+    // Show/hide
+
+    $('.call_info__number a').on( "click", function(e) {
+        e.preventDefault();
+        var id = $(this).attr('data-related');
+        $(".call_info__number").each(function(){
+            $(this).hide();
+            if($(this).attr('id') == id) {
+                $(this).show();
+            }
+        });
+    });
+
+});
+
+
